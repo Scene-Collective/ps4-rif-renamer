6ab5d00a2be55f42502619e8973d15be38e34c04
Al-Azif <alazif@miskatonic.io>
2021-01-02 23:06:41 -0800

    Overkill on "safe" functions

--- source/main.c
+++ source/main.c
@@ -6,11 +6,12 @@ int rename_rifs(const char *license_path) {
 
   if (dir) {
     struct dirent *dp;
-    char original_filename[255];
-    char new_filename[255];
     while ((dp = readdir(dir)) != NULL) {
+      char original_filename[PATH_MAX] = {0};
+      char new_filename[PATH_MAX] = {0};
+
       struct stat info;
-      sprintf(original_filename, "%s/%s", license_path, dp->d_name);
+      snprintf_s(original_filename, sizeof(original_filename), "%s/%s", license_path, dp->d_name);
 
       if (stat(original_filename, &info) == -1) {
         // Failed to stat object... skip it
@@ -19,18 +20,11 @@ int rename_rifs(const char *license_path) {
 
       // Rename if it's a file or symbolic link and starts with "fake"
       if ((S_ISREG(info.st_mode) || S_ISLNK(info.st_mode)) && strncmp("fake", dp->d_name, 4) == 0) {
-        char *modified_filename;
-
-        modified_filename = malloc(strlen(dp->d_name) + 1);
-        strcpy(modified_filename, dp->d_name);
-
-        modified_filename[1] = 0x72; // r
-        modified_filename[2] = 0x65; // e
-        modified_filename[3] = 0x65; // e
-
-        sprintf(new_filename, "%s/%s", license_path, modified_filename);
+        char modified_filename[PATH_MAX] = {0};
+        snprintf_s(modified_filename, sizeof(modified_filename), "%s", dp->d_name);
+        memmove_s(&modified_filename, sizeof(modified_filename), "free", 4);
+        snprintf_s(new_filename, sizeof(new_filename), "%s/%s", license_path, modified_filename);
         rename(original_filename, new_filename);
-        free(modified_filename);
       }
     }
     closedir(dir);
